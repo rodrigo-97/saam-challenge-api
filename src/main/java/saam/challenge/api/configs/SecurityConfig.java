d62f84f5d0920f70862e08108b4dd90253f43496
Rodrigo Araújo <rodrigo.dev99@gmail.com>
2025-12-10 04:02:18 -0300

    style: fix some code style stuff

--- src/main/java/saam/challenge/api/configs/SecurityConfig.java
+++ src/main/java/saam/challenge/api/configs/SecurityConfig.java
@@ -24,7 +24,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -52,8 +51,7 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-        ;
+                );
 
         return http.build();
     }
